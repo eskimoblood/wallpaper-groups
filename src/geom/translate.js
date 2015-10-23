63deb1a6fefae4315282c60f717f93b6fa20fc9c
Andreas Köberle <koeberle@gmail.com>
2015-10-23 21:51:01 +0200

    refactoring to be more functional

--- src/geom/translate.js
+++ src/geom/translate.js
@@ -1,38 +1,32 @@
 import { curry } from 'ramda';
 
-export const translate1W2H = curry((width, height, columns, i) =>({
-  x: width * (i % columns),
-  y: 2 * height * Math.floor(i / columns)
+const w = (width, i, columns) => width * (i % columns);
+const h = (height, i, columns) => height * Math.floor(i / columns);
+
+const translate = curry((fW, fH, width, height, columns, i)=>({
+  x: fW * w(width, i, columns),
+  y: fH * h(height, i, columns)
 }));
 
-export const translate2W1H = curry((width, height, columns, i) =>({
-  x: 2 * width * (i % columns),
-  y: height * Math.floor(i / columns)
-}));
-
-export const translate2W2H = curry((width, height, columns, i) =>({
-  x: 2 * width * (i % columns),
-  y: 2 * height * Math.floor(i / columns)
-}));
-
-export const translateShifted = curry((width, height, columns, i)=> {
+const _translateShifted = curry(curry((fH, width, height, columns, i)=> {
   var offsetX = 0;
   if (Math.floor(i / columns) % 2) {
     offsetX = width / 2;
   }
   return {
-    x: offsetX + width * (i % columns),
-    y: height * Math.floor(i / columns)
+    x: offsetX + w(width, i, columns),
+    y: h(height, i, columns)
   };
-});
+}));
 
-export const translateHex = curry((width, height, columns, i)=> {
-  var offsetX = 0;
-  if (Math.floor(i / columns) % 2) {
-    offsetX = width / 2;
-  }
-  return {
-    x: offsetX + width * (i % columns),
-    y: 0.75 * height * Math.floor(i / columns)
-  };
-});
+export const translate1W1H = translate(1, 1);
+
+export const translate1W2H = translate(1, 2);
+
+export const translate2W1H = translate(2, 1);
+
+export const translate2W2H = translate(2, 2);
+
+export const translateShifted = _translateShifted(1);
+
+export const translateHex = translateShifted(0.75);
